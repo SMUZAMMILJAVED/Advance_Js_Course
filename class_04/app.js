dc3f6e5066a413767bfc87525bb2bb7c510d8322
S MuzammiL Javed <121771009+SMUZAMMILJAVED@users.noreply.github.com>
2026-07-03 03:23:56 +0500

    promises, map , set

---
+++ class_04/app.js
@@ -0,0 +1,57 @@
+
+                           //Promises
+
+
+// function count(time) {
+//     return new Promise((resolve, reject) => {
+//         let interval = setInterval(() => {
+//             console.log("hello");
+
+//         }, [1000])
+//         setTimeout(() => {
+//             clearInterval(interval);
+//             console.log('clear interval');
+// resolve();
+//         }, time * 1000);
+        
+//     })
+// }
+// count(3)
+//     .then(() => { console.log('done!'); return count(3) })
+//     .then(() => { console.log("done 2"); })
+//     .catch((err) => { console.log(err); })
+
+
+                                   // set / Map
+
+
+// let data=new Set();
+// data.add(1)
+// data.add(1)
+// data.add(2)
+// data.add(5)
+// data.add(4)
+// data.delete(1)
+
+// console.log(data);
+// console.log(data.size);
+// for(let item of data){
+//     console.log(item);
+    
+// }
+// let pArr=[1,2,3,4,5,7,8,9,9,9,1,0]
+// let data=new Set(pArr);
+
+// data.add(10)
+// let arr=[...data]
+// // data.clear()
+// console.log(data);
+// console.log(data.has(2) );
+
+// console.log(arr);
+
+
+// let data=new Map();
+// data.set(4555,'belal')
+// data.set(4552,'hamza')
+// console.log(data);
